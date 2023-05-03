a0116218ff42f6bd04118dfe210da7ffe7db7121
InnsmouthFisherman <76620415+InnsmouthFisherman@users.noreply.github.com>
2023-05-03 21:35:13 +0500

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,18 +1,14 @@
 def merge_sort(nums):
-	if len(nums) > 1:
-		
+	if len(nums) > 1:	
 		# разделение списка на две части 
 		mid = len(nums) // 2
 		left = nums[:mid]
 		right = nums[mid:]
-		
 		merge_sort(left)
 		merge_sort(right)
-		
 		# i - позиция в левой половине j - позиция в правой половине 
 		# k - позиция в исходном массиве
 		i = j = k = 0
-		
 		# каждый элемент двух половин массива сранивается с элементом 
 		# из другой половины: если левое число больше, оно помещается 
 		# в nums на позицию k, i увеличивается на 1
@@ -25,20 +21,15 @@ def merge_sort(nums):
 				nums[k] = right[j]
 				j += 1
 			k += 1
-		
-		# сортировка оставшиъся элементов 
-		
+		# сортировка оставшихся элементов 
 		while i < len(left):
 			nums[k] = left[i]
 			i += 1
-			k += 1
-			
+			k += 1			
 		while j < len(right):
 			nums[k] = right[j]
 			j += 1
 			k += 1
-			
 		return nums 
 		
-
 print(merge_sort([2,15,23,3,3,7]))
